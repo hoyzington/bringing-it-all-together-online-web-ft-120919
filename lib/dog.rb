cea0e57135b04c2c091643b15af2af9c1f87c2de
Scott Hoisington <hoyzington@gmail.com>
2020-01-25 07:09:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -21,6 +21,7 @@ class Dog
     sql = "INSERT INTO dogs (name, breed) VALUES (?, ?)"
     DB[:conn].execute(sql, self.name, self.breed)
     @id = DB[:conn].execute("SELECT * FROM dogs")[0][0]
+    self
   end
   
   def self.create(name:, breed:)
